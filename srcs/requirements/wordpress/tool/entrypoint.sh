078f0ae4ec3fca8bcbe1c6c447fe2c22693af46b
Michele-Reymond <hello@michelereymond.ch>
2023-04-04 20:07:23 +0200

    ok

--- srcs/requirements/wordpress/tool/entrypoint.sh
+++ srcs/requirements/wordpress/tool/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-cat ./wordpress/wp-config.php
-
-if [ $? -eq 1 ]; then
+if [ ! -e ./wordpress/wp-config.php ]; then
     wget https://wordpress.org/latest.tar.gz
-    
-    sleep 1
     tar -xzvf latest.tar.gz
     rm -rf latest.tar.gz
 
@@ -14,8 +10,7 @@ if [ $? -eq 1 ]; then
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	
-    sleep 1
+
 	# active le fichier wp-config
 	mv wp-config-sample.php wp-config.php
     echo "Wordpress installed"
